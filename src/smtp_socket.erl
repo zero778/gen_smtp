f1ce2aa11ca88fc19e36a0281faac6859f6c82fe
JIANFENG <zero_x201@hotmail.com>
2019-08-10 10:23:24 +0800

    add ssl connection options ciphers with all

--- src/smtp_socket.erl
+++ src/smtp_socket.erl
@@ -48,6 +48,7 @@
                               {packet, line},
                               {ip, {0,0,0,0}},
                               {versions, ['tlsv1', 'tlsv1.1', 'tlsv1.2']},
+                              {ciphers, ssl:cipher_suites(all)},
                               {port, 0}]).
 
 -ifdef(TEST).
